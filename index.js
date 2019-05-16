ac57fea5f99d078aeffd17ec66b9b7990f009703
Simon Kocurek <simon.kocurek@gmail.com>
2019-05-16 19:30:44 +0200

    Update gitignore

--- index.js
+++
@@ -1,5 +0,0 @@
-export const InMemoryDataAdapter = require('./src/server_data_adapter');
-export const LocalStorageStore = require('./src/client_offline_store');
-export const Commands = require('./src/commands');
-export const Client = require('./src/client');
-export const Server = require('./src/server');
