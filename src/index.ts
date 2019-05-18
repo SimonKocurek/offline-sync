7497e396f8e86cba53d66597d411b22157541bed
SimonKocurek <simon.kocurek@gmail.com>
2019-05-18 18:11:18 +0200

    Update export structure

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Client from './client';
+import Server from './server';
+import InMemoryDataAdapter from './data_adapter/in_memory_data_adapter';
+import LocalStorageStore from './offline_store/local_storage_store';
+
+export {Client, Server, InMemoryDataAdapter, LocalStorageStore};
