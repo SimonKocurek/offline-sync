9d0649489165a1a162f0079e150b24bb84bc52fd
Simon Kocurek <simon.kocurek@gmail.com>
2019-05-18 13:35:24 +0200

    Remove unnecessary files

--- test/functions.ts
+++
@@ -1,9 +0,0 @@
-
-import { expect } from 'chai';
-
-describe('Stack', () => {
-    it('can be initialized without an initializer', () => {
-        const s: number[] = [];
-        expect(s.length).to.equal(0);
-    });
-});
