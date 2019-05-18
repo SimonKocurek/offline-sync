f53883e7f9baee7c3c7cd2316eb39036fbb3d52b
Simon Kocurek <simon.kocurek@gmail.com>
2019-05-18 18:13:07 +0200

    Remove index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    Client: require('dist/client'),
-    Server: require('dist/client'),
-    InMemoryDataAdapter: require('dist/data_adapter/in_memory_data_adapter'),
-    LocalStorageStore: require('dist/offline_store/local_storage_store')
-};
